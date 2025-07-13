eb97b6ebc5da8e634af0446829687c43e907a6fc
Aditya <adeeteya@gmail.com>
2025-07-13 19:41:37 -0400

    Adjust ball physics

--- lib/components/ball.dart
+++ lib/components/ball.dart
@@ -12,20 +12,38 @@ class Ball extends PositionComponent
   Ball({
     required this.color,
     required this.isBallRound,
-    required this.velocity,
+    required Vector2 velocity,
     super.size,
     super.position,
-  }) : super(anchor: Anchor.center);
+  })
+      : velocity = Vector2.copy(velocity),
+        _speed = velocity.length,
+        super(anchor: Anchor.center);
 
   final bool isBallRound;
   final Color color;
-  final Vector2 velocity;
+  Vector2 velocity;
+  double _speed;
+
+  void _normalizeSpeed() {
+    velocity
+      ..normalize()
+      ..scale(_speed);
+  }
+
+  void _increaseSpeed() {
+    _speed *= 1.05;
+    _normalizeSpeed();
+  }
 
   @override
   void render(Canvas canvas) {
     if (isBallRound) {
       canvas.drawCircle(
-          Offset(size.x / 2, size.x / 2), size.x / 2, color.filledPaint());
+        Offset(size.x / 2, size.x / 2),
+        size.x / 2,
+        color.filledPaint(),
+      );
     } else {
       canvas.drawRect(Offset.zero & size.toSize(), color.filledPaint());
     }
@@ -38,6 +56,7 @@ class Ball extends PositionComponent
     } else {
       add(RectangleHitbox());
     }
+    _normalizeSpeed();
   }
 
   @override
@@ -48,34 +67,42 @@ class Ball extends PositionComponent
 
   @override
   void onCollisionStart(
-      Set<Vector2> intersectionPoints, PositionComponent other) {
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
     super.onCollisionStart(intersectionPoints, other);
     if (other is ScreenHitbox) {
       if (intersectionPoints.first.y <= 0) {
         velocity.y = -velocity.y;
         game.playPing();
+        _increaseSpeed();
       } else if (intersectionPoints.first.y >= game.height) {
         velocity.y = -velocity.y;
         game.playPing();
+        _increaseSpeed();
       } else if (intersectionPoints.first.x <= 0) {
-        add(RemoveEffect(
+        add(
+          RemoveEffect(
             delay: 1,
             onComplete: () {
               game.rightPlayerPointWin();
-            }));
+            },
+          ),
+        );
       } else if (intersectionPoints.first.x >= game.width - 0.2) {
-        add(RemoveEffect(
+        add(
+          RemoveEffect(
             delay: 1,
             onComplete: () {
               game.leftPlayerPointWin();
-            }));
+            },
+          ),
+        );
       }
     } else if (other is Paddle) {
       velocity.x = -velocity.x;
       game.playPing();
-      //Movement of ball based on where it hit the paddle
-      velocity.y = velocity.y +
-          (position.y - other.position.y) / other.size.y * game.height * 0.3;
+      _increaseSpeed();
     }
   }
 }
